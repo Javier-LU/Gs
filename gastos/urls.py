4f2357e6c1d6fa9ad4ddd51246823e26d7f9629e
Javier Luque <135378603+Javier-LU@users.noreply.github.com>
2025-12-11 15:22:57 +0100

    Create urls.py

---
+++ gastos/urls.py
@@ -0,0 +1,8 @@
+# gastos/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),    
+    path("informes/", include("gastos.informes.urls")),
+]
